8477f5b747bbb021d226e60470597ce446af535e
DoanHoang2606 <109168075+DoanHoang2606@users.noreply.github.com>
2024-04-15 23:43:41 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+# ENCODE BY KHANHNGUYEN9872                                 # Write your comment here! 
+# Version OBF: v2.4.0                                       # Write your comment here! 
+# Botname: @KazutoBot_bot                                   # Write your comment here! 
+# Username Obf: @KIMANHVV2 (5637336841)                     # Write your comment here! 
+# Mode ENC: python3.12 - High (main)                        # Write your comment here! 
+# Date Obf: 2024-03-09 13:31:22 (UTC)                       # Write your comment here! 
+exec(__import__('zlib').decompress(__import__('base64').b64decode(b'p5YEQ/V/d8N49ue/82nLxs2b2
